dd7d4ca4998eb7d8ca5cf91f20b1afa35c619d73
Ada Chen <ch46234881@gmail.com>
2025-09-27 16:56:41 +0800

    資料測試

--- src/services/currentUser.js
+++ src/services/currentUser.js
@@ -1,4 +1,29 @@
+//假資料
+
+
 export const currentUser = {
-  memberId: 1,        // 假設目前登入的會員 ID
-  username: 'Ada'     // 假設目前登入的使用者名稱
+  memberId: 1,        
+  username: 'Ada'     
 }
+
+
+/*
+export const currentUser = {
+  memberId: 2,        
+  username: 'John'     
+}
+*/
+
+/*
+export const currentUser = {
+  memberId: 5,        
+  username: 'Sophia'     
+}
+*/
+
+/*
+export const currentUser = {
+  memberId: 11,        
+  username: 'Ethan'     
+}
+*/
